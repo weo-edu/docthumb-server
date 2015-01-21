f42f020264d544ef714665f98c0670bb617a7257
Andrew Shaffer <andrew@weo.io>
2015-01-21 15:06:35 -0800

    update

--- index.js
+++ index.js
@@ -1,23 +1,8 @@
 var app = require('express')();
-var obtain = require('./lib/obtain');
-var thumb = require('./lib/thumb');
 
-app.get('/', function(req, res) {
-  var url = req.query.url;
-  var page = Number(req.query.page) || 1;
+app.use(require('./lib/send-png'));
+app.use(require('./lib/main'));
 
-  obtain(url, function(err, fileName) {
-    if(err) throw err;
-
-    thumb(fileName, {page: page}, function(err, pngBuf) {
-      if(err) throw err;
-
-      res.header('Content-Type', 'image/png');
-      res.send(pngBuf);
-    });
- });
-});
-
-app.listen(80, function() {
+app.listen(Number(process.env.PORT) || 80, function() {
   console.log('listening');
 });
