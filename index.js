af3bf9d73e33f98a01a032d32230b13d5b97fa49
Andrew Shaffer <andrew@weo.io>
2015-01-21 14:13:10 -0800

    update

--- index.js
+++ index.js
@@ -1,26 +1,26 @@
 var app = require('express')();
 var docThumb = require('document-thumbnail');
 var obtain = require('./lib/obtain');
+var unoconv = require('unoconv');
 
+unoconv.listen();
 app.get('/', function(req, res) {
   var url = req.query.url;
 
-  console.log('obtaining', url);
   obtain(url, function(err, fileName) {
     if(err) throw err;
 
-    console.log('obtained', fileName);
-    docThumb(fileName, function(err, pngBuf) {
-      if(err) throw err;
-
+    unoconv.convert(fileName, 'png', {exportStr: 'PageRange=1-1'}, function(err, pngBuf) {
       res.header('Content-Type', 'image/png');
       res.send(pngBuf);
     });
+    // docThumb(fileName, function(err, pngBuf) {
+    //   if(err) throw err;
+
+    //   res.header('Content-Type', 'image/png');
+    //   res.send(pngBuf);
+    // });
   });
-});
-
-app.get('/pdf', function(req, res) {
-
 });
 
 app.listen(80, function() {
