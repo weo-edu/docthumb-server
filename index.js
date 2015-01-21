a1ec4e321877b1608a759d5dd6d519aed473c77b
Andrew Shaffer <andrew@weo.io>
2015-01-21 14:53:40 -0800

    update

--- index.js
+++ index.js
@@ -4,11 +4,12 @@ var thumb = require('./lib/thumb');
 
 app.get('/', function(req, res) {
   var url = req.query.url;
+  var page = Number(req.query.page) || 1;
 
   obtain(url, function(err, fileName) {
     if(err) throw err;
 
-    thumb(fileName, function(err, pngBuf) {
+    thumb(fileName, {page: page}, function(err, pngBuf) {
       if(err) throw err;
 
       res.header('Content-Type', 'image/png');
